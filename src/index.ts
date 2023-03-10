6a0fa4a845bd13d2c7137afe6c7a1dffc61df894
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-03-10 08:31:39 -0300

    Configuração e inicialização do servidor express

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-console.log("Olá Mundo")
+import { runServer } from "./main/server";
+import { DatabaseConnection } from "./main/database/typeorm.connection";
+
+// 1 Conectar ao DB, se sucesso subir a API
+Promise.all([DatabaseConnection.connect()])
+    .then(runServer)
+    .catch((error: any) => {
+        console.log("Erro ao iniciar");
+        console.log(error)
+    })
