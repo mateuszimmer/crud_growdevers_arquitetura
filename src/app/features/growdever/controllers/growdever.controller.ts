95479c02601714c6b8b5a2a2bc6c92e9d00c5214
Mateus Zimmer <102668946+mateuszimmer@users.noreply.github.com>
2023-03-11 13:36:53 -0300

    Criação do growdever.controler.ts e do http.helper.ts

---
+++ src/app/features/growdever/controllers/growdever.controller.ts
@@ -0,0 +1,26 @@
+import { Request, Response } from 'express'
+import { HttpHelper } from '../../../shared/util/http.helper';
+import { GrowdeverRepository } from '../repositories/growdever.repository';
+import { CreateGrowdeverUseCase } from '../usecases/create-growdever.usecase';
+
+export class GrowdeverController {
+    public async create(req: Request, res: Response) {
+        try {
+            const { nome, email } = req.body;
+
+            const useCase = new CreateGrowdeverUseCase(
+                new GrowdeverRepository()
+            );
+
+            const result = await useCase.execute({
+                nome,
+                email
+            });
+
+            return HttpHelper.sucess(res, result, 'OK', 201)
+
+        } catch (error: any) {
+            return HttpHelper.serverError(res, error)
+        }
+    }
+}
